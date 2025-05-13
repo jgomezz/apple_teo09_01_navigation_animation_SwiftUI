788467d130f3f90e878b10a05b8b229cfbe929df
jgomezz <jgomezz@gmail.com>
2025-05-12 21:02:53 -0500

    Initial Commit

---
+++ apple_teo09_01_navigation_animation_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apple_teo09_01_navigation_animation_SwiftUI
+//
+//  Created by Jaime Gomez on 12/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple_teo09_01_navigation_animation_SwiftUI/apple_teo09_01_navigation_animation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_teo09_01_navigation_animation_SwiftUIApp.swift
+//  apple_teo09_01_navigation_animation_SwiftUI
+//
+//  Created by Jaime Gomez on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct apple_teo09_01_navigation_animation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
